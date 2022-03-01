29540cbe356dbd65667fac559e561bac6728239e
hocinebouarara <hocine@univ-tiaret.dz>
2022-03-01 01:49:25 +0100

    create the close button

--- medicalOffice/src/login/LoginController.java
+++ medicalOffice/src/login/LoginController.java
@@ -5,9 +5,16 @@
  */
 package login;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +23,11 @@ import javafx.fxml.Initializable;
  */
 public class LoginController implements Initializable {
 
+    @FXML
+    private JFXTextField userNameFld;
+    @FXML
+    private JFXPasswordField passwordFld;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +35,16 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void close(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+
+    }
+
+    @FXML
+    private void Login(ActionEvent event) {
+    }
     
 }
